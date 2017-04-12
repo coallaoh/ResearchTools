26eb39eb51b6e1d362a0b7a834c51e93ea6ad6f3
joon <joon@mpi-inf.mpg.de>
2017-04-12 09:58:58 +0200

    Summing class for parallel summation

--- util/parallel.py
+++ util/parallel.py
@@ -6,3 +6,22 @@ def apply_async_wrapper(func, *args):
         return func(*args)
     except Exception as e:
         return e
+
+
+class Sum:
+    def __init__(self, shape, type=np.int):
+        self.value = np.zeros(shape, dtype=type)  # this is the initialization of the sum
+        self.lock = thread.allocate_lock()
+        self.count = 0
+
+    def add(self, value):
+        if type(value) == TypeError:
+            raise Exception("Error occurred inside parallel loop")
+        elif type(value) == IOError:
+            raise Exception("Loading failed:" + value.filename)
+
+        self.count += 1
+        self.lock.acquire()  # lock so sum is correct if two processes return at same time
+        self.value += value  # the actual summation
+        self.lock.release()
+
