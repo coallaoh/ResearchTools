76a38fda4463a4035e0803dbdabf83a6a76bda85
joon <joon@mpi-inf.mpg.de>
2017-09-28 15:33:22 +0200

    more control code

--- util/control.py
+++ util/control.py
@@ -3,16 +3,40 @@ __author__ = "joon"
 import pprint
 import platform
 import os
+import os.path as osp
+
+
+class filemanager(object):
+    def __init__(self, maindir, filename):
+        self.maindir = maindir
+        self.filename = filename
 
 
 class experiment_control(object):
-    def __init__(self, control, conf, default):
+    def __init__(self, control, conf, default, exclude=()):
         self.conf = conf
         self.control = control
         self._default = default
+        self._exclude = exclude
         self.control_token = self._apply_defaults(self.control, self._default)
+        self._apply_excludes(self.control_token, self._exclude)
         from util.construct_filenames import create_token
         self.token = create_token(self.control_token)
+        self._set_maindir()
+
+    def filemanager(self, filename, pathname, override=False):
+        path = osp.join(self.maindir, filename)
+        if not (self.conf['overridecache'] or override):
+            if osp.isfile(path):
+                from util.exceptions import CacheFileExists
+                raise CacheFileExists("File %s already exists!" % path)
+        self.dirs[pathname] = path
+
+    def _set_maindir(self):
+        self.maindir = osp.join('cache', self.conf['exp_phase'], self.token)
+        from util.ios import mkdir_if_missing
+        mkdir_if_missing(self.maindir)
+        self.dirs = {}
 
     def _apply_defaults(self, control, default_control):
         import copy
@@ -32,11 +56,21 @@ class experiment_control(object):
 
         return control_token
 
+    def _apply_excludes(self, control, exclude):
+        for ex in exclude:
+            ky = ex['condition'].keys()[0]
+            if control[ky] == ex['condition'][ky]:
+                control.pop(ex['remove'])
+
     def _print(self):
         print(":" * 50)
         print("Machine : %s" % platform.node())
         print("GPU : %d" % self.conf['gpu'])
         print("PID : %d" % os.getpid())
+        print(">>> Exp Name <<<")
+        print(self.conf['exp_phase'])
+        print(">>> Conf <<<")
+        pprint.pprint(self.conf)
         print(">>> Control <<<")
         pprint.pprint(self.control)
         print("Token : %s" % self.token)
